615edb512f2b18da9b896f95f10e6f7126542f29
sen2y <pse6704@sookmyung.ac.kr>
2024-06-20 01:00:39 +0900

    🔥 미사용 코드 삭제

--- src/components/common/organisms/Header.jsx
+++ src/components/common/organisms/Header.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import styled from "styled-components";
 import SearchBar from "@/components/search/atoms/SearchBar";
-import { LogoText, Column, Row } from "@/styles/commonStyles";
+import { LogoText, Row } from "@/styles/commonStyles";
 import HeaderBtn from "../molecules/HeaderBtn";
 
 export default function Header() {
@@ -43,21 +43,6 @@ const SmallLogoText = styled(LogoText)`
   font-size: 30px;
   text-shadow: 2px 2px 1px rgba(118, 118, 118, 0.2);
   align-items: center;
-`;
-
-const HeaderSearchBar = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 460px;
-  height: 48px;
-  padding-right: 20px;
-`;
-
-const HeaderColumn = styled(Column)`
-  display: flex;
-  align-items: flex-end;
-  justify-content: flex-start;
 `;
 
 const HeaderRow = styled(Row)`
