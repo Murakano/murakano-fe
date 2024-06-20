e75d6217115360fca23fbe6185e91fa87f9d7773
sen2y <pse6704@sookmyung.ac.kr>
2024-06-21 02:10:04 +0900

    💄 input, textarea 창 제외 페이지 내 드래그 방지 설정

--- src/pages/auth/requests.jsx
+++ src/pages/auth/requests.jsx
@@ -1,11 +1,12 @@
 import Header from "@/components/common/organisms/Header";
+import { Container } from "@/styles/commonStyles";
 
 export default function Requests() {
   return (
-    <div>
+    <Container>
       <Header />
       <h1>My Requests</h1>
       {/* 내 요청 목록 표시 */}
-    </div>
+    </Container>
   );
 }
