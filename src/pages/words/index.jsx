236786d37fb1c808ebf7c56b52a2c4ca73b15517
dev-Rhea <dev.rhea4444@gmail.com>
2024-06-24 15:56:57 +0900

    ✨ 💄 전체 단어 목록 타이틀

--- src/pages/words/index.jsx
+++ src/pages/words/index.jsx
@@ -1,12 +1,12 @@
 import Header from "@/components/common/organisms/Header";
 import { Container } from "@/styles/commonStyles";
+import WordsPageName from "@/components/words/atoms/WordsPageName";
 
 export default function AllWords() {
   return (
     <Container>
       <Header />
-      <h1>All Words</h1>
-      {/* 전체 단어 목록 표시 */}
+      <WordsPageName/>
     </Container>
   );
 }
