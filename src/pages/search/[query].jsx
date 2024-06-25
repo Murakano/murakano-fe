86b7a3aee24717fa85051be22c5b592f3b2b18a2
true <truee030@gmail.com>
2024-06-25 14:31:48 +0900

    💄전체 수정

--- src/pages/search/[query].jsx
+++ src/pages/search/[query].jsx
@@ -1,9 +1,12 @@
 // 코드예시.
 // 검색어에 따른 동적 라우팅 페이지
-
-import Header from '@/components/common/organisms/Header';
-import styled from 'styled-components';
-import { useRouter } from 'next/router';
+import Header from "@/components/common/organisms/Header";
+import styled from "styled-components";
+import { useRouter } from "next/router";
+import CategoryDate from "@/components/search/molecules/CategoryDate";
+import ResultBoxIntegration from "@/components/search/organisms/ResultBoxIntegration";
+import SorryText from "@/components/search/atoms/SorryText"; 
+import AddRequestBtn from  "@/components/search/atoms/AddRequestBtn";
 
 export default function SearchResults() {
   const router = useRouter();
@@ -12,10 +15,36 @@ export default function SearchResults() {
   return (
     <Container>
       <Header />
-      <Section>{query ? <h1>검색 결과: {query}</h1> : <h1>검색어를 입력해주세요.</h1>}</Section>
+      <Section>
+        {query ? (
+          <>
+            <CategoryDate />
+            <ResultWord>{query}</ResultWord>
+            <ResultBoxIntegration />
+          </>
+        ) : (
+          <>
+            <SorryText query={query} /> 
+            <AddRequestBtn />
+          </>
+        )}
+      </Section>
     </Container>
   );
 }
+
+const ResultWord = styled.div`
+  color: #000000;
+  width: 691px;
+  height: 102px;
+  padding: 10px;
+  gap: 29px;
+  font-size: 40px;
+  font-weight: 700;
+  line-height: 60px;
+  text-align: center;
+  letter-spacing: -0.03em;
+`;
 
 const Container = styled.div`
   display: flex;
