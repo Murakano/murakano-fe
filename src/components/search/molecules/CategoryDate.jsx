3abdaa172144edd0dd7f2319f82b38cf1e2acb4b
true <truee030@gmail.com>
2024-06-25 14:22:21 +0900

    💄카테고리, 수정 날짜 묶음

---
+++ src/components/search/molecules/CategoryDate.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styled from 'styled-components';
+import ResultCategory from '../atoms/ResultCategory';
+import ResultEditDate from '../atoms/ResultEditDate';
+
+export default function CategoryDate() {
+    return (
+      <Container>
+          <ResultCategory />
+          <ResultEditDate />
+      </Container>
+    );
+  }
+
+const Container = styled.div`
+  width: 782px;
+  height: 30.71px;
+  padding: 0px 51.5px 0px 51.5px; 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+
