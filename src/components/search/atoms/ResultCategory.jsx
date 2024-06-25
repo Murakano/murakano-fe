f18d5ca1e7b006159a4e822bdffd57a0559e3aa1
true <truee030@gmail.com>
2024-06-25 13:59:35 +0900

    💄카테고리 텍스트 추가

---
+++ src/components/search/atoms/ResultCategory.jsx
@@ -0,0 +1,36 @@
+// src/components/search/atoms/ResultCategory.jsx
+
+import React from 'react';
+import styled from 'styled-components';
+
+const ResultCategory = () => {
+  return (
+    <Container>
+      <Text>
+        카테고리: HTML, 브라우저
+      </Text>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 127px;
+  height: 29px;
+  padding: 10px 0;
+  gap: 10px;
+  border-bottom: 1px solid #CCCCCC;
+`;
+
+const Text = styled.div`
+  width: 182px;
+  height: 18px;
+  gap: 0px;
+
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 18px;
+  letter-spacing: -0.03em;
+  margin-right;
+`;
+
+export default ResultCategory;
