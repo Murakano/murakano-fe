62189876f240c84807ae4d1024698651ec625c29
dev-Rhea <dev.rhea4444@gmail.com>
2024-06-28 11:00:44 +0900

    💄 ✨ 드롭다운 추가

--- src/pages/words/index.jsx
+++ src/pages/words/index.jsx
@@ -1,7 +1,10 @@
 import Header from "@/components/common/organisms/Header";
+import styled from "styled-components";
 import { Container } from "@/styles/commonStyles";
 import WordsPageName from "@/components/words/atoms/WordsPageName";
 import WordList from "@/components/words/molecules/WordList";
+import WordDropdown from "@/components/words/molecules/WordDropdown";
+import ComDropdown from "@/components/common/molecules/ComDropdown";
 
 
 export default function AllWords() {
@@ -9,7 +12,19 @@ export default function AllWords() {
     <Container>
       <Header />
       <WordsPageName/>
+      <DropdownContainer>
+        <WordDropdown />
+        <ComDropdown />
+      </DropdownContainer>
       <WordList />
     </Container>
   );
 }
+
+const DropdownContainer = styled.div`
+    display: flex;
+    flex-start: left;
+    width: 691px;
+    height: 35px;
+    gap: 20px;
+`;
