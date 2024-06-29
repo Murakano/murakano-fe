566ecd98f8573303a90941b711a335de9f77eedf
jjikky <kok5417@skuniv.ac.kr>
2024-06-29 16:56:21 +0900

    🎨 prettier 적용

--- src/components/words/molecules/SortDropdown.jsx
+++ src/components/words/molecules/SortDropdown.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import CommDropdown from "@/components/common/molecules/CommDropdown";
+import React from 'react';
+import CommDropdown from '@/components/common/molecules/CommDropdown';
 
 export default function SortDropdown() {
   const labels = ['A-Z', 'Z-A', '인기순', '최신순'];
-  return(
+  return (
     <>
-    <CommDropdown labels={labels} dropdownName="전체"/>
+      <CommDropdown labels={labels} dropdownName='전체' />
     </>
   );
 }
