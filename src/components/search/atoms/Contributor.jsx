5a759a17a37b9279c837644d91ec2857bbd8d5e9
true <truee030@gmail.com>
2024-06-30 15:09:06 +0900

    🔥 gap, line-height등 불필요한 코드 삭제

--- src/components/search/atoms/Contributor.jsx
+++ src/components/search/atoms/Contributor.jsx
@@ -4,23 +4,18 @@ import styled from 'styled-components';
 
 const Contributor = () => {
     return (
-      <>
       <StyledContainer>
         <Text>
           기여자: 행복한 토마토
         </Text>
         </StyledContainer>
-      </>
     );
   };
 
   const Text = styled.div`
   width: 110px;
   height: 20px;
-  gap: 0px;
   font-size: 13px;
-  font-weight: 400;
-  line-height: 19.5px;
   letter-spacing: -0.03em;
   margin-right: auto;
 `;
