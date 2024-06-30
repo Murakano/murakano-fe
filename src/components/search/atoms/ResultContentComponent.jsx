c6d44b95291a758ac1d394198783a7bd7e79fa96
true <truee030@gmail.com>
2024-06-30 15:25:08 +0900

    🔥 gap, opacity 삭제

--- src/components/search/atoms/ResultContentComponent.jsx
+++ src/components/search/atoms/ResultContentComponent.jsx
@@ -5,12 +5,7 @@ import styled from 'styled-components';
 const StyledResultContent = styled.span`
   width: 600px;
   height: 20px;
-  gap: 0px;
-  opacity: 0px;
   font-size: 13px;
-  font-weight: 300;
-  line-height: 22.5px;
-  letter-spacing: -0.03em;
   text-align: left;
 `;
 
