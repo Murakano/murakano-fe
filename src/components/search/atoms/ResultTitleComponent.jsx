229c1f1aba6d666f78c7c166e24775be02973b33
true <truee030@gmail.com>
2024-06-30 15:29:33 +0900

    ♻️ 🔥 gap, opacity삭제, 태그 변경

--- src/components/search/atoms/ResultTitleComponent.jsx
+++ src/components/search/atoms/ResultTitleComponent.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 // Styled component 정의
-const StyledResultTitle = styled.span`
+const StyledResultTitle = styled.div`
   width: 600px;
   height: 23px;
-  gap: 0px;
-  opacity: 0px;
   font-size: 15px;
   font-weight: 600;
-  line-height: 22.5px;
-  letter-spacing: -0.03em;
   text-align: left;
 `;
 
