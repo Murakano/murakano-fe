420e311acc4e0bbc45f15bdcd843bdbe08643650
sen2y <pse6704@sookmyung.ac.kr>
2024-07-02 03:07:28 +0900

    💄 기여자 없는경우 미출력

--- src/components/search/molecules/ContributorEditBtn.jsx
+++ src/components/search/molecules/ContributorEditBtn.jsx
@@ -3,18 +3,19 @@ import styled from 'styled-components';
 import Contributor from '../atoms/Contributor';
 import ResultEditBtn from '../atoms/ResultEditBtn';
 
-export default function ContributorEditBtn() {
+export default function ContributorEditBtn({ searchResult }) {
+  const hasContributor = Boolean(searchResult.suggestedBy);
   return (
-    <StyledContainer>
-      <Contributor />
-      <ResultEditBtn />
+    <StyledContainer $hasContributor={hasContributor}>
+      {hasContributor ? <Contributor searchResult={searchResult} /> : null}
+      <ResultEditBtn searchResult={searchResult} />
     </StyledContainer>
   );
 }
 
 const StyledContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: ${(props) => (props.$hasContributor ? 'space-between' : 'flex-end')};
   align-items: flex-end;
   width: 691px;
   height: 61px;
