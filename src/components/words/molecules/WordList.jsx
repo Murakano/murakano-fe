0da3053cc5572af7b7f1155a7acd02bb60db388c
dev-Rhea <dev.rhea4444@gmail.com>
2024-07-01 15:14:09 +0900

    ✨ 무한 스크롤, 로컬 스토리지에 스크롤 위치 저장

--- src/components/words/molecules/WordList.jsx
+++ src/components/words/molecules/WordList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import WordItem from '../atoms/WordItem';
 import { useRouter } from 'next/router';
@@ -8,14 +8,49 @@ const wordDirectory = [
   { name: 'React', pron: '리액트' },
   { name: 'yml', pron: '야믈' },
   { name: 'Linux', pron: '리눅스' },
+  { name: 'PostgreSQL', pron: '포스트그래스, 포스트그래스큐엘' },
+  { name: 'GUI', pron: '구이' },
+  { name: 'deno', pron: '디노' },
+  { name: 'nginx', pron: '엔진엑스' },
+  { name: 'sudo', pron: '수두' },
+  { name: 'qt', pron: '큣' },
+  { name: 'DOM', pron: '돔' },
+  { name: 'React', pron: '리액트' },
+  { name: 'yml', pron: '야믈' },
+  { name: 'Linux', pron: '리눅스' },
+  { name: 'PostgreSQL', pron: '포스트그래스, 포스트그래스큐엘' },
+  { name: 'GUI', pron: '구이' },
+  { name: 'deno', pron: '디노' },
+  { name: 'nginx', pron: '엔진엑스' },
+  { name: 'sudo', pron: '수두' },
+  { name: 'qt', pron: '큣' },
+  { name: 'DOM', pron: '돔' },
+  { name: 'React', pron: '리액트' },
+  { name: 'yml', pron: '야믈' },
+  { name: 'Linux', pron: '리눅스' },
+  { name: 'PostgreSQL', pron: '포스트그래스, 포스트그래스큐엘' },
+  { name: 'GUI', pron: '구이' },
+  { name: 'deno', pron: '디노' },
+  { name: 'nginx', pron: '엔진엑스' },
+  { name: 'sudo', pron: '수두' },
+  { name: 'qt', pron: '큣' },
 ];
 
-// 무한 스크롤 변수
-const pageSize = 10;
-const loadMoreThreshold = 300;
+// 로컬 스토리지에 스크롤 저장 & 호출
+const saveScrollPosition = () => {
+  localStorage.setItem('scrollPosition', window.scrollY);
+};
+
+const loadScrollPosition = () => {
+  return parseInt(localStorage.getItem('scrollPosition'), 10) || 0;
+};
 
 export default function WordList() {
   const router = useRouter();
+  const [words, setWords] = useState([]);
+  const [page, setPage] = useState(0);
+  const observer = useRef();
+
   // 단어 목록 클릭 시 해당 단어 상세 페이지로 이동
   const handleWordClick = (name) => {
     if (name) {
@@ -23,10 +58,47 @@ export default function WordList() {
     }
   };
 
+  const lastWordElementRef = useCallback(
+    (node) => {
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          setPage((prevPage) => prevPage + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [page]
+  );
+
+  useEffect(() => {
+    const newWords = wordDirectory.slice(page * 10, (page + 1) * 10);
+    setWords((prev) => [...prev, ...newWords]);
+  }, [page]);
+
+  useEffect(() => {
+    // 초기 단어 호출
+    if (words.length === 0) {
+      const newWords = wordDirectory.slice(0, 10);
+      setWords(newWords);
+    }
+
+    // 스크롤 위치 복구
+    window.scrollTo(0, loadScrollPosition());
+
+    return () => {
+      saveScrollPosition();
+    };
+  }, []);
+
   return (
     <WordListContainer>
       {wordDirectory.map((word, index) => (
-        <WordListDiv key={index} onClick={() => handleWordClick(word.name)}>
+        <WordListDiv
+          key={index}
+          ref={index === words.length - 1 ? lastWordElementRef : null}
+          onClick={() => handleWordClick(word.name)}
+        >
           <WordItem name={word.name} pron={word.pron} />
         </WordListDiv>
       ))}
