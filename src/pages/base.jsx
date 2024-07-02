e4ea55e9e54cf15646c45f9ffad815adcd48aefa
sen2y <pse6704@sookmyung.ac.kr>
2024-07-02 11:26:11 +0900

    🔊 로그로그

--- src/pages/base.jsx
+++ src/pages/base.jsx
@@ -1,10 +1,13 @@
 import Header from '@/components/common/organisms/Header';
 import { Container } from '@/styles/commonStyles';
 import { useRouter } from 'next/router';
+import { getCookie } from '@/utils/getCookie';
 
 export default function BaseLayout({ children }) {
   const router = useRouter();
   const margintop = router.pathname === '/' ? '38px' : '130px';
+  const token = getCookie();
+  console.log(token, 'baseLayout', 999);
 
   return (
     <Container margintop={margintop}>
