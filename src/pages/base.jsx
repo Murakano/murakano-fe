7c6684db61d2e0cc2ba448d880d78ebb87bd8d57
sen2y <pse6704@sookmyung.ac.kr>
2024-07-02 11:30:53 +0900

    🔊

--- src/pages/base.jsx
+++ src/pages/base.jsx
@@ -2,12 +2,19 @@ import Header from '@/components/common/organisms/Header';
 import { Container } from '@/styles/commonStyles';
 import { useRouter } from 'next/router';
 import { getCookie } from '@/utils/getCookie';
+import { useEffect } from 'react';
 
 export default function BaseLayout({ children }) {
   const router = useRouter();
   const margintop = router.pathname === '/' ? '38px' : '130px';
-  const token = getCookie();
-  console.log(token, 'baseLayout', 999);
+
+  useEffect(() => {
+    const accessToken = getCookie();
+    console.log('base', accessToken, 1001);
+    if (!accessToken) {
+      console.log('base', accessToken, 1002);
+    }
+  }, []);
 
   return (
     <Container margintop={margintop}>
