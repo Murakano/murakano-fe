b40516d32689a416489108aec3a06f2f04a7fd54
true <truee030@gmail.com>
2024-07-05 23:13:13 +0900

    ♻️ 🔥 모달 초기화 및 코드 초기화

--- src/components/search/atoms/ResultEditBtn.jsx
+++ src/components/search/atoms/ResultEditBtn.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
-import Modal from "@/components/auth/organisms/Modal";
+import EditRequestModal from "@/components/search/organisms/EditRequestModal"; // 여기서 Modal을 EditRequestModal로 변경
 import ActionBtn from '@/components/common/atoms/ActionBtn'; 
 
 const ResultEditBtn = () => {
@@ -17,7 +17,7 @@ const ResultEditBtn = () => {
   return (
     <>
       <AddRequestBtn onClick={handleOpenModal}>수정 요청</AddRequestBtn>
-      {isModalOpen && <Modal onClose={handleCloseModal} />}
+      {isModalOpen && <EditRequestModal onClose={handleCloseModal} />} {/* 여기서 Modal을 EditRequestModal로 변경 */}
     </>
   );
 };
