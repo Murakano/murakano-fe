799ebf9ec57636bfeb6eeddc05f5ef087ce69867
sen2y <pse6704@sookmyung.ac.kr>
2024-07-07 19:35:26 +0900

    🐛 검색결과 페이지, 전체용어페이지 등 페이지 이동시 검색어가 있으면 드롭다운이 열려서 렌더링되는문제 해결

--- src/components/search/organisms/SearchBar.jsx
+++ src/components/search/organisms/SearchBar.jsx
@@ -9,10 +9,10 @@ import { useSearchTermStore } from '@/store/useSearchTermStore';
 
 export default function SearchBar({ header }) {
   const router = useRouter();
-
   const searchBarRef = useRef();
   const debounceTimeoutRef = useRef(null);
 
+  const [firstRender, setFirstRender] = useState(true);
   const [relatedItems, setRelatedItems] = useState([]);
   const { searchTerm } = useSearchTermStore();
   // 검색어와 드롭다운 표시 여부를 관리하는 상태
@@ -48,19 +48,24 @@ export default function SearchBar({ header }) {
       // 새로운 타이머를 설정합니다.
       debounceTimeoutRef.current = setTimeout(async () => {
         const data = await fetchRelatedItems(searchTerm);
-        if (data.length) {
+        if (data?.length && !firstRender) {
           setDropdownVisible(true);
         } else {
           setDropdownVisible(false);
         }
       }, 300);
-
+      if (router.pathname !== `/search/${searchTerm}`) {
+        setDropdownVisible(false);
+      }
       // cleanup 함수: 컴포넌트가 언마운트되거나 searchTerm이 변경될 때 실행됩니다.
       return () => {
         clearTimeout(debounceTimeoutRef.current);
       };
+    } else {
+      setDropdownVisible(false);
+      setFirstRender(false);
     }
-  }, [searchTerm]);
+  }, [searchTerm, router.pathname]);
 
   const handleSearch = (e) => {
     if (e.key === 'Enter' || e.type === 'click') {
