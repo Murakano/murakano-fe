18e8a999760fb33b5dc6c4176bd0b184a192379d
dev-Rhea <dev.rhea4444@gmail.com>
2024-07-07 15:46:00 +0900

    🔥 스크롤저장 함수 삭제

--- src/components/words/molecules/WordList.jsx
+++ src/components/words/molecules/WordList.jsx
@@ -1,116 +1,84 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+WordList.jsx;
+
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import WordItem from '../atoms/WordItem';
 import { useRouter } from 'next/router';
 import TopScrollBtn from '@/components/common/atoms/TopScrollBtn';
 import api from '@/utils/api';
 
-// 스크롤 위치를 로컬 스토리지에 저장
-const saveScrollPosition = () => {
-  localStorage.setItem('scrollPosition', window.scrollY);
-};
-
-// 로컬 스토리지에서 스크롤 위치를 불러오기
-const loadScrollPosition = () => {
-  return parseInt(localStorage.getItem('scrollPosition'), 10) || 0;
-};
-
 export default function WordList() {
   const router = useRouter();
-  const [words, setWords] = useState([]);
   const [page, setPage] = useState(0);
+  const [words, setWords] = useState([]);
   const [loading, setLoading] = useState(false);
-  const observer = useRef();
+  const loaderRef = useRef(null);
+  const observerRef = useRef(null);
 
   // 단어 목록 클릭 시 해당 단어 상세 페이지로 이동
   const handleWordClick = (name) => {
     if (name) {
-      saveScrollPosition(); // 단어 항목 클릭시, 스크롤 위치 저장
       router.push(`/search/${name}`);
     }
   };
 
-  const lastWordElementRef = useCallback(
-    (node) => {
-      if (observer.current) observer.current.disconnect();
-      observer.current = new IntersectionObserver((entries) => {
-        if (entries[0].isIntersecting) {
-          setPage((prevPage) => prevPage + 1);
-        }
+  const fetchWords = async (page) => {
+    setLoading(true);
+    try {
+      const response = await api.get('/words', {
+        Sort: 'recent',
+        page: page + 1,
+        limit: 10,
       });
-      if (node) observer.current.observe(node);
-    },
-    [page]
-  );
+      const data = response.data;
+      setWords((prev) => [...prev, ...data]);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchWords = async () => {
-      setLoading(true);
-      try {
-        const data = await api.get('/word', {
-          Sort: 'recent',
-          page: page + 1,
-          limit: 10,
-        });
-        setWords((prev) => [...prev, ...data]);
-      } catch (error) {
-        console.error(error);
-      } finally {
-        setLoading(false);
-      }
-    };
-    fetchWords();
+    fetchWords(page);
   }, [page]);
 
   useEffect(() => {
-    // 스크롤 위치 복구
-    const savedScrollPosition = loadScrollPosition();
-    window.scrollTo(0, savedScrollPosition);
-
-    // 페이지 언로드 직전에 스크롤위치 저장
-    const savePositionHandler = () => saveScrollPosition();
-    window.addEventListener('beforeunload', savePositionHandler);
-
-    const popStateHandler = () => {
-      const savedScrollPosition = loadScrollPosition();
-      window.scrollTo(0, savedScrollPosition);
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 1.0,
     };
-    window.addEventListener('popstate', popStateHandler);
 
-    // visibility API , 페이지가 숨겨지거나 다시 보여질 때 스크롤 위치 저장 및 복구
-    const visiblitySaveHandler = () => {
-      if (document.visibilityState === 'hidden') {
-        saveScrollPosition();
-      } else {
-        const savedScrollPosition = loadScrollPosition();
-        window.scrollTo(0, savedScrollPosition);
+    const observer = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting && !loading) {
+        setPage((prevPage) => prevPage + 1);
       }
-    };
-    document.addEventListener('visibilitychange', visiblitySaveHandler);
+    }, options);
+
+    observerRef.current = observer;
+
+    if (loaderRef.current) {
+      observer.observe(loaderRef.current);
+    }
 
     return () => {
-      window.removeEventListener('beforeunload', savePositionHandler);
-      window.removeEventListener('popstate', popStateHandler);
-      document.removeEventListener('visibilitychange', visiblitySaveHandler);
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+      }
     };
-  }, []);
-
-  useEffect(() => {
-    return () => {};
-  }, []);
+  }, [loading]);
 
   return (
     <WordListContainer>
       {words.map((word, index) => (
-        <WordListDiv
-          key={word._id || index}
-          ref={index === words.length - 1 ? lastWordElementRef : null}
-          onClick={() => handleWordClick(word.word)}
-        >
+        <WordListDiv key={word._id || index} onClick={() => handleWordClick(word.word)}>
           <WordItem name={word.word} pron={word.comPron} />
         </WordListDiv>
       ))}
-      {loading && <p>Loading</p>}
+      <div ref={loaderRef} id='loader' style={{ height: '50px', marginTop: '10px' }}>
+        {loading ? 'Loading...' : ''}
+      </div>
       <ScrollContainer>
         <TopScrollBtn />
       </ScrollContainer>
