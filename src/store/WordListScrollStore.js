9abbb68275d1a93d961aff30c799dd7be3f44d6b
dev-Rhea <dev.rhea4444@gmail.com>
2024-07-11 21:09:23 +0900

    🔥 Zustand 적용 취소

--- src/store/WordListScrollStore.js
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-
-export const WordListScrollStore = create((set) => ({
-  scrollPosition: 0,
-  setScrollPosition: (position) => set({ scrollPosition: position }),
-}));
-
-WordListScrollStore.subscribe((scrollPosition) => console.log('Zustand scrollPosition updated:', scrollPosition));
