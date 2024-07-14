334a1fc56ef07004337bdce04de8209d9106d72a
true <truee030@gmail.com>
2024-07-15 01:56:28 +0900

    ♻️ 물음표 아이콘 컴포넌트화

---
+++ src/components/search/molecules/IconContainer.jsx
@@ -0,0 +1,13 @@
+// IconContainer.jsx
+import styled from 'styled-components';
+
+const IconContainer = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center; /* 중앙 정렬 */
+  &:hover .tooltip {
+    visibility: visible;
+  }
+`;
+
+export default IconContainer;
---
+++ src/components/search/molecules/LabelWithIcon.jsx
@@ -0,0 +1,10 @@
+// LabelWithIcon.jsx
+import styled from 'styled-components';
+
+const LabelWithIcon = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 3px; // 간격 조정
+`;
+
+export default LabelWithIcon;
