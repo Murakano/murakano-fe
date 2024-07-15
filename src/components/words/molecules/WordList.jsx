e92204166ea40a02fd333424a7fa91a8fc45d5f2
dev-Rhea <dev.rhea4444@gmail.com>
2024-07-15 21:56:22 +0900

    ⚡️ 고유 키 생성

--- src/components/words/molecules/WordList.jsx
+++ src/components/words/molecules/WordList.jsx
@@ -7,7 +7,7 @@ export default function WordList({ words = [], handleWordClick, lastWordElementR
     <WordListContainer>
       {words.map((word, index) => (
         <WordListDiv
-          key={word._id || index}
+          key={`${word._id}-${index}`} // 고유한 키 생성
           onClick={() => handleWordClick(word.word)} // 인덱스를 전달
           ref={words.length === index + 1 ? lastWordElementRef : null}
         >
